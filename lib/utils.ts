caa61a105df4a82bee4ef8976daab546581e685f
YB122 <youssefbenyamine2eme@gmail.com>
2026-05-07 14:01:59 +0300

    add log fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,5 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function getApiUrl(path: string = "") {
   const baseUrl =
     process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000"
+  console.log(baseUrl, path)
+
   return `${baseUrl}${path}`
 }
